127f35e10c3754a88c61aa57d8d5fd147f8b4c6d
Lorry Lu <lupeng5@xiaomi.com>
2019-09-15 21:15:46 +0800

    test5

--- auto_docs_deploy.sh
+++ auto_docs_deploy.sh
@@ -2,15 +2,23 @@
 BASEDIR=`dirname $0`
 BASEDIR=`(cd "$BASEDIR"; pwd)`
 
-echo "Start to deploy articles onto github page..."
+echo "# Start to upload mk format files to github ..."
 MSG=$1
 echo "- mkdocs build"
 mkdocs build --clean
 echo "- mkdocs gh-deploy"
 mkdocs gh-deploy
-echo "update source code"
+echo "# update source code"
 git add -A
 git commit -m "$MSG"
 git push
+echo "# mk format files are uploaded..."
+echo "# End of uploading mk format files."
 
-echo "Done"
+echo "# Start to deploy HTML files into poplp.github.io"
+echo "# Check poplp.github.io project is cloned or not."
+
+cd ../../
+pwd
+
+echo "# End of deploy HTML files."
