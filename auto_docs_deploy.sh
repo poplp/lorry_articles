5237a55cdea3106f77b9a44365dfec90696b4862
Lorry Lu <lupeng5@xiaomi.com>
2019-09-15 20:44:25 +0800

    test4

--- auto_docs_deploy.sh
+++ auto_docs_deploy.sh
@@ -5,7 +5,7 @@ BASEDIR=`(cd "$BASEDIR"; pwd)`
 echo "Start to deploy articles onto github page..."
 MSG=$1
 echo "- mkdocs build"
-mkdocs build
+mkdocs build --clean
 echo "- mkdocs gh-deploy"
 mkdocs gh-deploy
 echo "update source code"
