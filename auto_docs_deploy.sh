0ee4471a1708ac7ef23dd26ddcfe4be245799785
Lorry Lu <lupeng5@xiaomi.com>
2019-09-15 21:16:25 +0800

    test6

--- auto_docs_deploy.sh
+++ auto_docs_deploy.sh
@@ -6,8 +6,6 @@ echo "# Start to upload mk format files to github ..."
 MSG=$1
 echo "- mkdocs build"
 mkdocs build --clean
-echo "- mkdocs gh-deploy"
-mkdocs gh-deploy
 echo "# update source code"
 git add -A
 git commit -m "$MSG"
@@ -18,7 +16,7 @@ echo "# End of uploading mk format files."
 echo "# Start to deploy HTML files into poplp.github.io"
 echo "# Check poplp.github.io project is cloned or not."
 
-cd ../../
+cd ../
 pwd
 
 echo "# End of deploy HTML files."
