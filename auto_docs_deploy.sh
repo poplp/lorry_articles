8b5021f228e2e4e1e14c812d9dfd7564b7ae2c7a
Lorry Lu <lupeng5@xiaomi.com>
2019-09-15 15:30:31 +0800

    test

--- auto_docs_deploy.sh
+++ auto_docs_deploy.sh
@@ -4,8 +4,11 @@ BASEDIR=`(cd "$BASEDIR"; pwd)`
 
 echo "Start to deploy articles onto github page..."
 MSG=$1
+echo "- mkdocs build"
 mkdocs build
+echo "- mkdocs gh-deploy"
 mkdocs gh-deploy
+echo "update source code"
 git add -A
 git commit -m "$MSG"
 git push
