e2cefc9a1a80c6cd0f2b05923be4e77d036a4177
Lorry Lu <lupeng5@xiaomi.com>
2019-09-21 12:51:43 +0800

    dd

--- expect.sh
+++ expect.sh
@@ -3,7 +3,7 @@ set SOURCE_FILE [lindex $argv 0]
 set PASSWORD [lindex $argv 1]
 set ALIYUN_HOST_PATH "root@116.62.206.98:/usr/share/nginx/html/"
 
-spawn scp -r $SOURCE_FILE/\* $ALIYUN_HOST_PATH
+spawn scp -r $SOURCE_FILE/ $ALIYUN_HOST_PATH
 expect {
     "password"
     {
